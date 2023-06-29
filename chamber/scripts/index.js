20a848680e548e1dcfdb34e2be9e7870089bd60f
Broncelino <bsojn27@gmail.com>
2023-06-29 01:58:57 -0600

    lesson4 initial commit

---
+++ chamber/scripts/index.js
@@ -0,0 +1,12 @@
+
+function getDate() {
+    const datefield = document.getElementById("date");
+    const now = new Date();
+    const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+        now
+    );
+    const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+        dateStyle: "full"
+    }).format(now);
+    datefield.innerHTML = fulldate
+}
